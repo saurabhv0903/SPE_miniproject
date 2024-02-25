2e96c5de2ee46474b5537e60473db6d44d29951b
saurabhv0903 <saurabhvarade123@gmail.com>
2024-02-25 10:25:43 +0530

    using user input

--- calculator_main.py
+++ calculator_main.py
@@ -1,4 +1,3 @@
-import sys
 import math
 
 def square_root(x):
@@ -17,17 +16,14 @@ def power(x, b):
     return math.pow(x, b)
 
 def main():
-    if len(sys.argv) < 3:
-        print("Usage: python3 calculator_main.py <option> <value>")
-        print("Options:")
-        print("1. Square Root (√x)")
-        print("2. Factorial (x!)")
-        print("3. Natural Logarithm (ln(x))")
-        print("4. Power Function (x^b)")
-        sys.exit(1)
+    print("Options:")
+    print("1. Square Root (√x)")
+    print("2. Factorial (x!)")
+    print("3. Natural Logarithm (ln(x))")
+    print("4. Power Function (x^b)")
 
-    option = int(sys.argv[1])
-    value = float(sys.argv[2])
+    option = int(input("Enter your option: "))
+    value = float(input("Enter the value: "))
 
     if option == 1:
         print("Square Root of", value, "is:", square_root(value))
@@ -36,13 +32,11 @@ def main():
     elif option == 3:
         print("Natural Logarithm of", value, "is:", natural_log(value))
     elif option == 4:
-        if len(sys.argv) < 4:
-            print("Usage: python3 calculator_main.py 4 <base> <exponent>")
-            sys.exit(1)
-        exponent = float(sys.argv[3])
+        exponent = float(input("Enter the exponent: "))
         print(value, "raised to the power", exponent, "is:", power(value, exponent))
     else:
         print("Invalid option!")
 
 if __name__ == "__main__":
     main()
+
--- calculator_test.py
+++ calculator_test.py
@@ -4,28 +4,28 @@ import calculator_main
 class TestCalculator(unittest.TestCase):
 
     def test_square_root(self):
-        self.assertAlmostEqual(calculator_main.square_root(16), 4.0)   # Square root of 16 is 4
-        self.assertAlmostEqual(calculator_main.square_root(25), 5.0)   # Square root of 25 is 5
-        self.assertAlmostEqual(calculator_main.square_root(4), 2.0)    # Square root of 4 is 2
-        self.assertAlmostEqual(calculator_main.square_root(9), 3.0)    # Square root of 9 is 3
+        self.assertAlmostEqual(calculator_main.square_root(16), 4.0)   
+        self.assertAlmostEqual(calculator_main.square_root(25), 5.0)   
+        self.assertAlmostEqual(calculator_main.square_root(4), 2.0)    
+        self.assertAlmostEqual(calculator_main.square_root(9), 3.0)    
 
     def test_factorial(self):
-        self.assertEqual(calculator_main.factorial(0), 1)     # Factorial of 0 is 1
-        self.assertEqual(calculator_main.factorial(1), 1)     # Factorial of 1 is 1
-        self.assertEqual(calculator_main.factorial(5), 120)   # Factorial of 5 is 120
-        self.assertEqual(calculator_main.factorial(6), 720)   # Factorial of 6 is 720
+        self.assertEqual(calculator_main.factorial(0), 1)     
+        self.assertEqual(calculator_main.factorial(1), 1)     
+        self.assertEqual(calculator_main.factorial(5), 120)   
+        self.assertEqual(calculator_main.factorial(6), 720)   
 
     def test_natural_log(self):
-        self.assertAlmostEqual(calculator_main.natural_log(1), 0.0)          # Natural log of 1 is 0
-        self.assertAlmostEqual(calculator_main.natural_log(2), 0.6931471806) # Natural log of 2 is approximately 0.6931
-        self.assertAlmostEqual(calculator_main.natural_log(3), 1.0986122886) # Natural log of 3 is approximately 1.0986
-        self.assertAlmostEqual(calculator_main.natural_log(5), 1.6094379124) # Natural log of 5 is approximately 1.6094
+        self.assertAlmostEqual(calculator_main.natural_log(1), 0.0)          
+        self.assertAlmostEqual(calculator_main.natural_log(2), 0.6931471806) 
+        self.assertAlmostEqual(calculator_main.natural_log(3), 1.0986122886) 
+        self.assertAlmostEqual(calculator_main.natural_log(5), 1.6094379124) 
 
     def test_power(self):
-        self.assertAlmostEqual(calculator_main.power(2, 3), 8.0)     # 2 raised to the power 3 is 8
-        self.assertAlmostEqual(calculator_main.power(4, 2), 16.0)    # 4 raised to the power 2 is 16
-        self.assertAlmostEqual(calculator_main.power(5, 2), 25.0)    # 5 raised to the power 2 is 25
-        self.assertAlmostEqual(calculator_main.power(4, 3), 64.0)    # 4 raised to the power 3 is 64
+        self.assertAlmostEqual(calculator_main.power(2, 3), 8.0)     
+        self.assertAlmostEqual(calculator_main.power(4, 2), 16.0)    
+        self.assertAlmostEqual(calculator_main.power(5, 2), 25.0)    
+        self.assertAlmostEqual(calculator_main.power(4, 3), 64.0)    
 
 if __name__ == '__main__':
     unittest.main()
